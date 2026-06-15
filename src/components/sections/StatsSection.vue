11de7dfcc5a2943018689815e3449125412633c4
ArchyScript <yungdansa@gmail.com>
2026-06-15 15:23:26 +0100

    fioxed klanding page update

--- src/components/sections/StatsSection.vue
+++ src/components/sections/StatsSection.vue
@@ -7,7 +7,7 @@ import ImageLoader from '@/components/common/ImageLoader.vue';
   <LandingSection>
     <div class="container-page container-y-padding flex flex-col gap-8">
       <h2
-        class="-mt-8 max-w-[29rem] font-display text-4xl font-bold leading-[1.08] text-neutral-100 sm:text-5xl lg:text-6xl"
+        class="-mt-8 max-w-[40rem] font-display text-4xl font-bold leading-[1.08] text-neutral-100 sm:text-5xl lg:text-6xl"
       >
         Trusted by <span class="text-primary-100">4000+</span> Africans in the
         diaspora
