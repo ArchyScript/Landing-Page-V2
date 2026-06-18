4f7dc57694ed0e2f0c0e43c8441dc388328b2649
ArchyScript <yungdansa@gmail.com>
2026-06-18 09:38:26 +0100

    updated button size

--- src/components/common/AppButton.vue
+++ src/components/common/AppButton.vue
@@ -15,7 +15,7 @@ withDefaults(
 
 <template>
   <a
-    class="button-lift inline-flex items-center justify-center gap-2 rounded-md bg-primary-100 px-6 py-3 md:px-7 md:py-4 text-sm font-medium leading-5 !text-white transition duration-300 ease-smooth ring ring-primary-light"
+    class="button-lift inline-flex items-center justify-center gap-2 rounded-sm bg-primary-100 px-4 py-[10px] md:px-4 md:py-3 text-sm font-medium leading-5 !text-white transition duration-300 ease-smooth ring ring-primary-light"
     :href="href"
     :target="target"
     :rel="target === '_blank' ? 'noopener noreferrer' : undefined"
